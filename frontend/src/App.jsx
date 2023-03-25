0ee6faf61f0e26c129d80b0d84c9f1d1564474f1
Nuwan Kirillawala <nuwan.sv.kirillawala@gmail.com>
2023-03-25 16:35:27 +0530

    Add navbar and initial assets

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,19 @@
 import { useState } from 'react'
-import './App.css'
-
+import './App.scss'
+import { About, Header, Footer, Skills, Testimonials, Work } from './container/index'
+import { Navbar } from './components/index'
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className="App">
-      Hello
+    <div className="app">
+      <Navbar />
+      <Header />
+      <About />
+      <Work />
+      <Skills />
+      <Testimonials />
+      <Footer />
     </div>
   )
 }
---
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,22 @@
+import './Navbar.scss'
+import { images } from '../../constants/index'
+
+const Navbar = () => {
+  return (
+    <nav className='app__navbar'>
+      <div className='app__navbar-logo'>
+        <img src={images.logo} alt="logo" />
+      </div>
+      <ul className='app__navbar-links'>
+        {['home', 'about', 'contact', 'skills', 'work'].map(item => (
+          <li key={`link-${item}`} className='app__flex p-text'>
+            <div />
+            <a href={`#${item}`}>{item}</a>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  )
+}
+
+export default Navbar
