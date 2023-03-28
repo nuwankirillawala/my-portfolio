ec948be1e5bb1eb48c7bf7d3189a764baace670c
Nuwan Kirillawala <nuwan.sv.kirillawala@gmail.com>
2023-03-28 19:15:50 +0530

    Separate AboutProfileCard and remove backend

--- frontend/src/container/About/About.jsx
+++ frontend/src/container/About/About.jsx
@@ -1,18 +1,10 @@
 import './About.scss';
 import { motion } from 'framer-motion';
-import { useState, useEffect } from 'react';
-import { urlFor, client } from '../../client';
 import { AppWrap, MotionWrap } from '../../wrapper';
+import ProfileCard from './ProfileCard';
+import { images } from '../../constants';
 
 const About = () => {
-  const [abouts, setAbouts] = useState([]);
-
-  useEffect(() => {
-    const query = '*[_type == "abouts"]';
-
-    client.fetch(query)
-      .then((data) => setAbouts(data));
-  }, [])
 
   return (
     <>
@@ -25,19 +17,31 @@ const About = () => {
       </h2>
 
       <div className="app__profiles">
-        {abouts.map((about, index) => (
-          <motion.div
-            whileInView={{ opacity: 1 }}
-            whileHover={{ scale: 1.1 }}
-            transition={{ duration: 0.5, type: 'tween' }}
-            className='app__profile-item'
-            key={about.title + index}
-          >
-            <img src={urlFor(about.imgUrl)} alt={about.title} />
-            <h2 className="bold-text" style={{ marginTop: 20 }}>{about.title}</h2>
-            <p className="p-text" style={{ marginTop: 10 }}>{about.description}</p>
-          </motion.div>
-        ))}
+        <ProfileCard 
+        title='test'
+        description='testdes'
+        imgUrl={images.about03}
+        />
+        <ProfileCard 
+        title='test'
+        description='testdes'
+        imgUrl={images.about03}
+        />
+        <ProfileCard 
+        title='test'
+        description='testdes'
+        imgUrl={images.about03}
+        />
+        <ProfileCard 
+        title='test'
+        description='testdes'
+        imgUrl={images.about03}
+        />
+        <ProfileCard 
+        title='test'
+        description='testdes'
+        imgUrl={images.about03}
+        />
       </div>
     </>
   )
