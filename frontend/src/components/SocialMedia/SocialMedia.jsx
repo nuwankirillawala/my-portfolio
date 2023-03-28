c74ec685848ae3c641f9fe947d6bf131765f266f
Nuwan Kirillawala <nuwan.sv.kirillawala@gmail.com>
2023-03-28 16:29:40 +0530

    Modify Social Media icons

--- frontend/src/components/SocialMedia/SocialMedia.jsx
+++ frontend/src/components/SocialMedia/SocialMedia.jsx
@@ -1,16 +1,27 @@
-import {BsTwitter, BsInstagram} from 'react-icons/bs';
+import {BsGithub, BsMedium, BsLinkedin} from 'react-icons/bs';
 import {FaFacebook} from 'react-icons/fa';
 const SocialMedia = () => {
   return (
     <div className='app__social'>
         <div>
-            <BsTwitter />
+          <a href="https://www.linkedin.com/in/nuwan-kirillawala-0b275018b/" target='_blank'>
+          <BsLinkedin />
+          </a>
         </div>
         <div>
-            <BsInstagram />
-        </div>
-        <div>
+            <a href="https://web.facebook.com/nuwan.kirillawala" target='_blank'>
             <FaFacebook />
+            </a>
+        </div>
+        <div>
+            <a href="https://github.com/nuwankirillawala" target='_blank'>
+            <BsGithub />
+            </a>
+        </div>
+        <div>
+          <a href="functionater.medium.com" target='_blank'>
+            <BsMedium />
+          </a>
         </div>
     </div>
   )
