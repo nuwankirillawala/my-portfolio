e4394990d04edcba22ef0f623075313b48e0b939
Nuwan Kirillawala <nuwan.sv.kirillawala@gmail.com>
2023-03-28 09:10:56 +0530

    Add motion wrapper

--- frontend/src/container/Skills/Skills.jsx
+++ frontend/src/container/Skills/Skills.jsx
@@ -1,7 +1,7 @@
 import './Skills.scss'
 import { useState, useEffect, Fragment } from 'react'
 import { motion } from 'framer-motion';
-import { AppWrap } from '../../wrapper';
+import { AppWrap, MotionWrap } from '../../wrapper';
 import { Tooltip } from 'react-tooltip';
 import 'react-tooltip/dist/react-tooltip.css';
 import { urlFor, client } from '../../client';
@@ -90,4 +90,8 @@ const Skills = () => {
   )
 }
 
-export default AppWrap(Skills, 'skills');
+export default AppWrap(
+  MotionWrap(Skills, 'app__skills'),
+  'skills',
+  "app__whitebg"
+);
