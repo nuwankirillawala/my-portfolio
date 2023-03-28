d88fc288da3be1f6dbbc6a663a64dd7a0bbfa453
Nuwan Kirillawala <nuwan.sv.kirillawala@gmail.com>
2023-03-28 08:57:53 +0530

    Ad skills and experience section

--- frontend/src/container/Skills/Skills.jsx
+++ frontend/src/container/Skills/Skills.jsx
@@ -1,20 +1,21 @@
 import './Skills.scss'
-import { useState, useEffect } from 'react'
+import { useState, useEffect, Fragment } from 'react'
 import { motion } from 'framer-motion';
 import { AppWrap } from '../../wrapper';
-// import { ReactTooltip } from 'react-tooltip';
+import { Tooltip } from 'react-tooltip';
+import 'react-tooltip/dist/react-tooltip.css';
 import { urlFor, client } from '../../client';
 
 const Skills = () => {
-  const [experience, setExperience] = useState([]);
+  const [experiences, setExperiences] = useState([]);
   const [skills, setSkills] = useState([])
   useEffect(() => {
     const skillQuery = '*[_type == "skills"]';
-    const experienceQuery = '*[_type == "experience"]';
+    const experienceQuery = '*[_type == "experiences"]';
 
     client.fetch(experienceQuery)
       .then((data) => {
-        setExperience(data);
+        setExperiences(data);
       })
 
     client.fetch(skillQuery)
@@ -25,7 +26,7 @@ const Skills = () => {
 
 
   return (
-    <>
+    <Fragment>
       <h2 className="head-text">Skills & Experience</h2>
       <div className="app__skills-container">
         <motion.div className='app__skills-list'>
@@ -44,9 +45,49 @@ const Skills = () => {
           ))
           }
         </motion.div>
+
+        <div className="app__skills-exp">
+          {experiences.map((experience) => (
+            <motion.div
+              className="app__skills-exp-item"
+              key={experience.year}
+            >
+              <div className="app__skills-exp-year">
+                <p className="bold-text">{experience.year}</p>
+              </div>
+              <motion.div className="app__skills-exp-works">
+                {console.log(experience.works)}
+                {experience.works.map((work) => (
+                  <Fragment key={work.name}>
+                    <motion.div
+                      whileInView={{ opacity: [0, 1] }}
+                      transition={{ duration: 0.5 }}
+                      className="app__skills-exp-work"
+                      data-tooltip-id={work.name}
+                      key={work.name}
+                    >
+                      <h4 className="bold-text">{work.name}</h4>
+                      <p className="p-text">{work.company}</p>
+                      
+                    </motion.div>
+                    <Tooltip
+                      effect="solid"
+                      arrowColor="#fff"
+                      className="skills-tooltip"
+                      id={work.name}
+                      // place='bottom'
+                      key={work.name}
+                      content={work.desc}
+                    />
+                  </Fragment>
+                ))}
+              </motion.div>
+            </motion.div>
+          ))}
+        </div>
       </div>
-    </>
+    </Fragment>
   )
 }
 
-export default Skills
+export default AppWrap(Skills, 'skills');
